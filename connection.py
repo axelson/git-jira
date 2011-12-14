16a9aaa200a0ffdf85e851debebdf08d6a61bb34
Jason Axelson <jason.axelson@gmail.com>
2011-12-14 05:35:23 -1000

    [dev] Fix recursive login problem
    Change-Id: I40847abd2768f49257a01c477f65bcf23db4c28b

--- connection.py
+++ connection.py
@@ -9,6 +9,7 @@ from cookies import *
 '''This is a connection to jira'''
 class connection:
     def __init__(self):
+        #print "New connection"
         self.cookie = cookieHandler()
         #self.ensureLogin()
 
@@ -19,9 +20,15 @@ class connection:
         #print "ensure login"
         if not(self.checkLogin()):
             self.doLogin()
+            if not(self.checkLogin()):
+                print "There was a stale cookie file, trying one more time after clearing session cookies"
+                self.cookie.clearSessionCookies()
+                self.cookie.saveCookies()
+                self.doLogin()
 
     def getLoginUrl(self):
-        loginUrl = 'http://' + getGitValue('url') + '/rest/auth/latest/session'
+        #loginUrl = 'http://' + getGitValue('url') + '/rest/auth/latest/session'
+        loginUrl = 'http://' + getGitValue('url') + '/rest/auth/1/session'
         return loginUrl
 
     def checkLogin(self):
@@ -46,30 +53,29 @@ class connection:
         password = getGitValue('password')
         #password = getpass.getpass()
         #password = 'hunter2'
-        print "Logging in with %s and %s" % (username, password)
-        #TODO: Use correct url
+        loginUrl = self.getLoginUrl()
+        #print "Logging in with %s and %s" % (username, password)
+        print "Logging in to JIRA (%s) as %s" % (loginUrl, username)
 
         if (self.checkLogin()):
             # Already logged in
-            print "Already logged in"
+            # This method shouldn't be called if you are already logged in,
+            # instead you should call ensureLogin if unsure
+            print "ERROR: Already logged in, you should never see this in production!"
             return
 
-        loginUrl = self.getLoginUrl()
-        print "Logging in to JIRA (%s) as %s" % (loginUrl, username)
         txdata = '{"username" : "' + username +'", "password" : "'+ password +'"}'
         txheaders =  {'Content-Type' : 'application/json'}
         req = self.cookie.Request(loginUrl, txdata, txheaders)
         try:
             handle = self.cookie.urlopen(req)
         except self.cookie.HTTPError as inst:
-            print "Unable to login due to < %s >" % inst
-            self.cookie.clearSessionCookies()
-            self.cookie.saveCookies()
+            #print "Unable to login due to < %s >" % inst
             if (inst.code == 401):
                 #TODO: Check if this works when a cookie is expired
-                print "Possible stale session (HTTP 401), attempting to login again"
-                print "New session disabled"
-                self.doLogin()
+                print "Unable to login due to 401 error, possible stale cookie or shared cookie file"
+                #print "Possible stale session (HTTP 401), attempting to login again"
+                #print "New session disabled"
                 return
 
         print handle.read()
